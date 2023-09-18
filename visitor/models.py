4cd6af194255b1e373b7876a745d3bcebef0707a
christopherece <christopheranchetaece@gmail.com>
2023-09-18 23:15:51 +1200

    Fixed Name instead of email

--- visitor/models.py
+++ visitor/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class PersonToVisit(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class Visitor(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    email = models.EmailField()
     person_to_visit = models.ForeignKey(PersonToVisit, on_delete=models.CASCADE)
 
     def __str__(self):
